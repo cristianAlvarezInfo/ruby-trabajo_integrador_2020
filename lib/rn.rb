e42ab1ff3b7576fd229768bcef176e43aeb66219
cristian <cris122.lp@gmail.com>
2021-01-18 18:43:41 -0300

    Update rn.rb

--- lib/rn.rb
+++ lib/rn.rb
@@ -3,8 +3,8 @@ module RN
   autoload :Commands, 'rn/commands'
   autoload :PathResolver,  'helpers/helpers'
   autoload :Validator,  'helpers/helpers'
-  autoload :Book,  'models/bookClass'
-  autoload :Note,  'models/noteClass'
+  autoload :Book,  'models/book'
+  autoload :Note,  'models/note'
   # Agregar aquí cualquier autoload que sea necesario para que se cargue las clases y
   # módulos del modelo de datos.
   # Por ejemplo:
