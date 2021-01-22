506c598a343d80a2ee758c1512cde23549cb55ad
cristian <cris122.lp@gmail.com>
2021-01-21 23:31:00 -0300

    Update book.rb

--- rails/EntregaFinal/app/models/book.rb
+++ rails/EntregaFinal/app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user, inverse_of: :books
   has_many :notes, dependent: :destroy,inverse_of: :book
 
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: {maximum: 255},uniqueness: { scope: :user_id}
+ 
 
   def to_s
     title
