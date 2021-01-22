ad778f83924f596c43d79ceee2413ecb68d95926
cristian <cris122.lp@gmail.com>
2021-01-22 19:51:07 -0300

    Update routes.rb

--- rails/EntregaFinal/config/routes.rb
+++ rails/EntregaFinal/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root "main#home"
+  get '/export/:id', to: 'notes#export', as: 'export'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
