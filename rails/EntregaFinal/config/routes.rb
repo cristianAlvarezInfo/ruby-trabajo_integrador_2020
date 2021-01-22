7deff4612c4c7c8f2e39139acb96c5056f1cccc4
cristian <cris122.lp@gmail.com>
2021-01-21 23:32:08 -0300

    Update routes.rb

--- rails/EntregaFinal/config/routes.rb
+++ rails/EntregaFinal/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :books
+  
+  resources :books do
+    resources :notes
+  end
+  
   devise_for :users
   root "main#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
