4a37e3acbc0132064bca621a3ae0bc97244b5327
cristian <cris122.lp@gmail.com>
2021-01-21 23:31:05 -0300

    Update note.rb

--- rails/EntregaFinal/app/models/note.rb
+++ rails/EntregaFinal/app/models/note.rb
@@ -1,8 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :book, inverse_of: :notes
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: {maximum: 255},uniqueness: { scope: :book_id }
+ 
   validates :content, presence: true
-
   def to_s 
     title
   end
