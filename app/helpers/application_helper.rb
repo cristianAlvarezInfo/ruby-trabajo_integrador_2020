d865c0053ea961a3a1dbac74f66b0b428dc8f153
cristian <cris122.lp@gmail.com>
2021-01-29 20:44:32 -0300

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def self.return_book(user,book_id)
+        begin
+            book= user.books.find(book_id)
+            return book
+        rescue
+            raise ActionController::RoutingError.new('Not Found')
+          end
+          
+    end
 end
